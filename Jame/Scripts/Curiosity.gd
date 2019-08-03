53f1236456382cc44ab05e956207a6ec92086f42
MXN97 <mx.nothnagel@gmail.com>
2019-08-03 14:10:25 +0200

    Add basic physics and movement to player

--- Jame/Scripts/Curiosity.gd
+++ Jame/Scripts/Curiosity.gd
@@ -3,12 +3,33 @@ extends KinematicBody2D
 
 enum State {IDLE, MOVING}
 
+const GRAVITY = 50.0
+const WALK_ACCEL = 10.0
+const MAX_SPEED = 50.0
+
 var current_state = State.IDLE
+var velocity = Vector2()
 
 func _ready():
 	$AnimationPlayer.play("IDLE")
 	$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
 	InputController.connect("morse", self, "_on_morse")
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	
+	match current_state:
+		State.MOVING:
+			velocity.x += delta * WALK_ACCEL
+			velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+		State.IDLE:
+			velocity.x = 0
+	
+	var collision_vector = move_and_slide(velocity, Vector2(0, -1))
+	
+	# Reset gravitational forces when touching the ground
+	if collision_vector.y == 0:
+		velocity.y = 0
 
 func _on_animation_finished(animation):
 	match current_state:
