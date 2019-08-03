f8993529eb284cd36702285f6df9ba542c7b28bc
MXN97 <mx.nothnagel@gmail.com>
2019-08-03 21:08:03 +0200

    Reduce waiting time for jump activation

--- Jame/Scripts/Curiosity.gd
+++ Jame/Scripts/Curiosity.gd
@@ -21,7 +21,7 @@ func _ready():
 	InputController.connect("morse", self, "_on_morse")
 
 func _physics_process(delta):	
-	if is_jumping && $AnimationPlayer.current_animation_position >= 1.2:
+	if is_jumping && $AnimationPlayer.current_animation_position >= 1.0:
 		velocity.y = JUMP_POWER
 		is_jumping = false
 		is_midair = true
