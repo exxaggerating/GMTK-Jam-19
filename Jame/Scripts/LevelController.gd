3c2f5dcc088c6e4609a8047d809354637db62e83
MXN97 <mx.nothnagel@gmail.com>
2019-08-03 22:49:22 +0200

    Update level1
    Reorder some nodes
    Place goal
    Fix goal script

--- Jame/Scripts/LevelController.gd
+++ Jame/Scripts/LevelController.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready():
-	$GameWindow/DeathZone.connect("body_entered", self, "_on_death_zone")
+	$GameWindow/Bounds/DeathZone.connect("body_entered", self, "_on_death_zone")
 	$GameWindow/Goal.connect("body_entered", self, "_on_goal_reached")
 
 func _on_death_zone(other):
