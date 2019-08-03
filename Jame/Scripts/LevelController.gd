05aaace7e5e434ef198c5b739330a6f679e1fb0a
MXN97 <mx.nothnagel@gmail.com>
2019-08-03 22:25:23 +0200

    Add dummy goal mechanic to LevelController

--- Jame/Scripts/LevelController.gd
+++ Jame/Scripts/LevelController.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 func _ready():
 	$GameWindow/DeathZone.connect("body_entered", self, "_on_death_zone")
+	$GameWindow/Goal.connect("body_entered", self, "_on_goal_reached")
 
 func _on_death_zone(other):
 	if other.is_in_group("PLAYER"):
 		print("ded")
+
+func _on_goal_reached(other):
+	if other.is_in_group("PLAYER"):
+		print("QUEUE CUTSCENE GODDAMIT")
