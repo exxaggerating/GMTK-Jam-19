ef5c064f6f03a8eba1546ce44774b816445114b8
Delta Lemon <lukas.vavra12@gmx.at>
2019-08-03 13:26:24 +0200

    Change MainMenu fonts and styles

--- Jame/Scripts/MainMenu.gd
+++ Jame/Scripts/MainMenu.gd
@@ -10,17 +10,15 @@ var menu_items
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Check if there is already a Game file
-	menu_items = [$Panel, $Panel2, $Panel3]
-	var savefile_exists = false
-	if savefile_exists:
+	menu_items = [$StartGamePanel, $SelectLevelPanel, $QuitPanel]
+	if SceneSwitcher.completed_level != -1:
 		$StartGame.text = "Continue"
-		$Panel2.set("custom_styles/panel", index4)
+		$SelectLevelPanel.set("custom_styles/panel", index4)
 		menu = [0, 1, 2]
 	else:
 		$StartGame.text = "Start Game"
-		$Panel2.set("custom_styles/panel", index1)
+		$SelectLevelPanel.set("custom_styles/panel", index1)
 		menu = [0, 2]
-	var bg = ButtonGroup.new()
 
 func update_selection():
 	$Selection.rect_position.y = menu_items[menu[selection]].rect_position.y + 20
@@ -38,8 +36,7 @@ func backward():
 func select():
 	match menu[selection]:
 		0:
-			# Load the next/first Level scene
-			pass
+			SceneSwitcher.start_game()
 		1:
 			# Load select level scene
 			pass
