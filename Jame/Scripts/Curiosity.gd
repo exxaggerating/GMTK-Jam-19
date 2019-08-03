887158e9d025136ff945ef6cfbb541a7ca95c034
MXN97 <mx.nothnagel@gmail.com>
2019-08-03 13:20:33 +0200

    Create basic player script to control animations

---
+++ Jame/Scripts/Curiosity.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+enum State {IDLE, MOVING}
+
+var current_state = State.IDLE
+
+func _ready():
+	$AnimationPlayer.play("IDLE")
+	$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
+
+func _on_animation_finished(animation):
+	match current_state:
+		State.IDLE:
+			$AnimationPlayer.play("IDLE")
+		State.MOVING:
+			$AnimationPlayer.play("WALK")
+
+func _on_morse(Code, actions):
+	match actions:
+		[Code.SHORT]:
+			$AnimationPlayer.play("JUMP")
+		[Code.SHORT, Code.SHORT]:
+			if current_state == State.IDLE:
+				current_state = State.MOVING
+			else:
+				if $AnimationPlayer.current_animation == "WALK":
+					$AnimationPlayer.play("IDLE")
+				
+				current_state = State.IDLE
