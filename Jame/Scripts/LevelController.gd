9877cfc278088e636b5f49f07aaf081e13a37514
Delta Lemon <lukas.vavra12@gmx.at>
2019-08-03 22:23:37 +0200

    Add Shaders and Stuff

--- Jame/Scripts/LevelController.gd
+++ Jame/Scripts/LevelController.gd
@@ -6,4 +6,5 @@ func _ready():
 
 func _on_death_zone(other):
 	if other.is_in_group("PLAYER"):
-		print("ded")
+		$Tween.interpolate_property($Menu/Node2D/CameraWhiteNoise.material, "shader_param/cutoff", 0.0, 1.1, 2.0, Tween.TRANS_CIRC, Tween.EASE_IN)
+		$Tween.start()
